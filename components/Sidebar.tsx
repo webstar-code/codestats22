31321fed6e429c58c97e37cdef1720f5d8f79e30
webstar-code <bhaveshrc2001@gmail.com>
2022-01-27 19:10:17 +0530

    added profile and sidebar UI fixes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
-import React from "react";
+import React, { useContext } from "react";
+import { ReactContext } from "../context/context";
 
 
 const Sidebar = () => {
+  const state = useContext(ReactContext);
   return (
-    <div className=" w-64 h-full border-r bg-white py-4 px-10">
+    <div className="w-64 flex-shrink-0 h-full border-r bg-white py-4 px-10">
       <div className="flex flex-col items-center justify-center">
-        <div className="w-32 h-32 bg-blue-600"></div>
-        <h1>Webstar</h1>
+        <div className="w-32 h-32 bg-blue-600 rounded-full overflow-hidden mb-5">
+          <img src={state?.user?.photo} className="w-full object-cover" alt="profile_photo" />
+        </div>
+        <h1 className="text-lg whitespace-nowrap font-semibold text-primary">{state?.user?.display_name}</h1>
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <Link href={"/day"}><p className="w-full py-5 hover:bg-indigo-400 text-center cursor-pointer">Today</p></Link>
-        <Link href={"/week"}><p className="w-full py-5 hover:bg-indigo-400 text-center cursor-pointer">This week</p></Link>
-        <p className="w-full py-5 hover:bg-indigo-400 text-center cursor-pointer">This month</p>
-        <p className="w-full py-5 hover:bg-indigo-400 text-center cursor-pointer">This year</p>
+      <div className="flex flex-col items-center justify-center my-5">
+        <Link href={"/day"}><p className="w-full py-5 hover:bg-indigo-200 text-lg font-medium text-center cursor-pointer">Today</p></Link>
+        <Link href={"/week"}><p className="w-full py-5 hover:bg-indigo-200 text-lg font-medium text-center cursor-pointer">This week</p></Link>
+        <p className="w-full py-5 hover:bg-indigo-200 text-lg font-medium text-center cursor-pointer">This month</p>
+        <p className="w-full py-5 hover:bg-indigo-200 text-lg font-medium text-center cursor-pointer">This year</p>
 
       </div>
     </div>
